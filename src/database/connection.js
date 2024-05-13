113c3d3fced159fbcb0f7a4cb23ddf65ddfc36c6
PrinceDubey <princedubey1998@gmail.com>
2024-05-13 14:50:59 +0530

    updates

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,15 +4,15 @@ const { Sequelize } = require("sequelize-cockroachdb");
 require('dotenv').config()
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL,
+  // process.env.DATABASE_URL,
 
-  // process.env.DB_DATABASE,
-  // process.env.DB_USERNAME,
-  // process.env.DB_PASSWORD, 
-  // {
-  //   host: process.env.DB_HOST,
-  //   dialect: process.env.DB_DIALECT
-  // }
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  }
 )
 
 sequelize.authenticate()
