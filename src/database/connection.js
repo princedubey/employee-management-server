471a480da47bb2137029c5fe6a90ba7219b28e05
PrinceDubey <princedubey1998@gmail.com>
2024-05-13 17:42:56 +0530

    fix/issue

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,40 +1,40 @@
-// const { Sequelize } = require('sequelize')
-const { Sequelize } = require("sequelize-cockroachdb");
+// // const { Sequelize } = require('sequelize')
+// const { Sequelize } = require("sequelize-cockroachdb");
 
-require('dotenv').config()
+// require('dotenv').config()
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL,
+// const sequelize = new Sequelize(
+//   process.env.DATABASE_URL,
 
-  // process.env.DB_DATABASE,
-  // process.env.DB_USERNAME,
-  // process.env.DB_PASSWORD, 
-  // {
-  //   host: process.env.DB_HOST,
-  //   dialect: process.env.DB_DIALECT
-  // }
-)
+//   // process.env.DB_DATABASE,
+//   // process.env.DB_USERNAME,
+//   // process.env.DB_PASSWORD, 
+//   // {
+//   //   host: process.env.DB_HOST,
+//   //   dialect: process.env.DB_DIALECT
+//   // }
+// )
 
-try {
-  sequelize.authenticate().then(() => {
-    console.log('Database connection has been established successfully.')
-  })
-} catch (error) {
-  console.error('Unable to connect to the database:', error)
-}
+// try {
+//   sequelize.authenticate().then(() => {
+//     console.log('Database connection has been established successfully.')
+//   })
+// } catch (error) {
+//   console.error('Unable to connect to the database:', error)
+// }
 
 
-module.exports = sequelize
+// module.exports = sequelize
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL);
+// // const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-// (async () => {
-//   try {
-//     const [results, metadata] = await sequelize.query("SELECT NOW()");
-//     console.log(results);
-//   } catch (err) {
-//     console.error("error executing query:", err);
-//   } finally {
-//     await sequelize.close();
-//   }
-// })();
+// // (async () => {
+// //   try {
+// //     const [results, metadata] = await sequelize.query("SELECT NOW()");
+// //     console.log(results);
+// //   } catch (err) {
+// //     console.error("error executing query:", err);
+// //   } finally {
+// //     await sequelize.close();
+// //   }
+// // })();
