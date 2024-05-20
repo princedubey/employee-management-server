98f606cc6c1bfd3adfde6c0b1cc62ad20cbb3960
PrinceDubey <princedubey1998@gmail.com>
2024-05-20 20:45:48 +0530

    error-code

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -1,41 +1,39 @@
-const jwt = require('jsonwebtoken')
-const usersServiceProvider = require('../services/users-service-provider')
+const jwt = require('jsonwebtoken');
+const usersServiceProvider = require('../services/users-service-provider');
 
 exports.validateAccessToken = async (req, res, next) => {
   try {
-    const accessToken = req.headers.authorization
+    const accessToken = req.headers.authorization;
     if (!accessToken) {
-      return res.status(409).json({ 
+      return res.status(401).json({
         message: 'Invalid Access Token!',
         error_code: 'INVALID_ACCESS_TOKEN'
-      })
+      });
     }
-  
-    jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
+
+    jwt.verify(accessToken, process.env.JWT_SECRET, async (err, decoded) => {
       if (err) {
-        return res.status(409).json({ 
+        return res.status(401).json({
           message: 'Invalid Access Token!',
           error_code: 'INVALID_ACCESS_TOKEN'
-        })
+        });
       }
-      req.user = decoded
-    })
 
-    console.log(accessToken)
+      const userId = decoded.id;
 
-    const userId = req.user.id
-    const user = await usersServiceProvider.getUserById(userId)
-    if (accessToken !== user.access_token) {
-      return res.status(409).json({ 
-        message: 'Invalid Access Token!',
-        error_code: 'INVALID_ACCESS_TOKEN'
-      })
-    }
+      const user = await usersServiceProvider.getUserById(userId);
 
-    req.user = user
+      if (!user || accessToken !== user.access_token) {
+        return res.status(401).json({
+          message: 'Invalid Access Token!',
+          error_code: 'INVALID_ACCESS_TOKEN'
+        });
+      }
 
-    next()
+      req.user = user;
+      next();
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
